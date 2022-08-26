80e7739893cc2bf7465fee3ab727340d1e787772
Aliaksei Stralkou <aliaksei.stralkou@innowise-group.com>
2022-08-26 17:45:22 +0300

    Interface Segregation Principle

---
+++ 1_SOLID/isp.py
@@ -0,0 +1,101 @@
+"""Interface Segregation Principle (ISP)."""
+
+
+from abc import abstractmethod
+
+
+class Machine:
+    """Our base class with printer methods."""
+    def print(self, document):
+        raise NotImplementedError()
+
+    def fax(self, document):
+        raise NotImplementedError()
+
+    def scan(self, document):
+        raise NotImplementedError()
+
+
+# ok if you need a multifunction device
+class MultiFunctionPrinter(Machine):
+    """Modern printer which have all following methods."""
+    def print(self, document):
+        pass
+
+    def fax(self, document):
+        pass
+
+    def scan(self, document):
+        pass
+
+
+class OldFashionedPrinter(Machine):
+    """Old printer which dont have modern methods like 'fax' and 'scan'.
+    And we can pass operator or raise Exception to fill the method's void.
+    But user can still see all of those methods."""
+    def print(self, document):
+        # ok - print stuff
+        pass
+
+    def fax(self, document):
+        pass  # do-nothing
+
+    def scan(self, document):
+        """Not supported!"""
+        raise NotImplementedError('Printer cannot scan!')
+
+
+# ?The best way is create separate methods and combine it and use only available methods to any printers
+class Printer:
+    @abstractmethod
+    def print(self, document):
+        pass
+
+
+class Scanner:
+    @abstractmethod
+    def scan(self, document):
+        pass
+
+
+# same for Fax, etc.
+
+class MyPrinter(Printer):
+    def print(self, document):
+        print(document)
+
+
+class Photocopier(Printer, Scanner):
+    def print(self, document):
+        print(document)
+
+    def scan(self, document):
+        pass  # something meaningful
+
+
+# ?Combine all methods in one modern machine
+class MultiFunctionDevice(Printer, Scanner):  # , Fax, etc
+    @abstractmethod
+    def print(self, document):
+        pass
+
+    @abstractmethod
+    def scan(self, document):
+        pass
+
+
+class MultiFunctionMachine(MultiFunctionDevice):
+    def __init__(self, printer, scanner):
+        self.printer = printer
+        self.scanner = scanner
+
+    def print(self, document):
+        self.printer.print(document)
+
+    def scan(self, document):
+        self.scanner.scan(document)
+
+
+printer = OldFashionedPrinter()
+printer.fax(123)  # nothing happens
+printer.scan(123)  # oops!
