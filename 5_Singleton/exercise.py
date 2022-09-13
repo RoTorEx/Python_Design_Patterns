a7b3bd1ce9421a2dbfeac768b9c81dcebb3772ee
Aliaksei Stralkou <aliaksei.stralkou@innowise-group.com>
2022-09-13 16:19:48 +0300

    pattern: Singleton

---
+++ 5_Singleton/exercise.py
@@ -0,0 +1,22 @@
+"""Task for the Singleton pattern
+
+Since it is very easy to implement a singleton, you will be tasked with implementing the is_singleton() function.
+This method takes a factory method as an argument and you need to determine if this factory method produces
+the same object or not. Return True if yes, False if not."""
+
+
+"""
+# *Start template
+def is_singleton(factory):
+    # ToDo: call factory() and return true or false
+    # depending on whether the factory makes a
+    # singleton or not
+"""
+
+
+# ?Solution
+def is_singleton(factory):
+    # ToDo: call factory() and return true or false
+    # depending on whether the factory makes a
+    # singleton or not
+    pass
---
+++ 5_Singleton/tempCodeRunnerFile.py
@@ -0,0 +1,7 @@
+False
+
+    # def __init__(self):
+    #     # self.id = random.randint(1,101)
+    #     # print('Generated an id of ', self.id)
+    #     # print('Loading database from file')
+    #     pass
