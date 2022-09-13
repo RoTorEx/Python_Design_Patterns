b5f5799b4de8074cdeb705795d247a9aa1255d47
Aliaksei Stralkou <aliaksei.stralkou@innowise-group.com>
2022-09-13 11:59:58 +0300

    pattern: Builder: exercise

--- 2_Builder/exercise.py
+++ 2_Builder/exercise.py
@@ -1,16 +1,18 @@
 """ Pattern task Builder
-It is necessary to implement a template constructor to form pieces of code.
 
+It is necessary to implement a template constructor to form pieces of code.
 An example of using the API you need to support:
 
 cb = CodeBuilder('Person').add_field('name', '""').add_field('age', '0')
 print(cb)
+
 Estimate exmaple output:
 
 class Person:
   def __init__(self):
     self.name = ""
     self.age = 0
+
 Dont forget about tabulation.
 
 Example with empty class:
@@ -27,29 +29,52 @@ class Foo:
 # *Start template
 class CodeBuilder:
     def __init__(self, root_name):
-        # todo
+        # ToDo
         pass
 
     def add_field(self, type, name):
-        # todo
+        # ToDo
         pass
 
     def __str__(self):
-        # todo
+        # ToDo
         pass
 """
 
 
 # ?Solution
-class CodeBuilder:
-    def __init__(self, root_name):
-        # todo
-        pass
-
-    def add_field(self, type, name):
-        # todo
-        pass
+class Field:
+    def __init__(self, name, value):
+        self.value = value
+        self.name = name
 
     def __str__(self):
-        # todo
-        pass
+        return 'self.%s = %s' % (self.name, self.value)
+
+
+class Class:
+    def __init__(self, name):
+        self.name = name
+        self.fields = []
+
+    def __str__(self):
+        lines = ['class %s:' % self.name]
+        if not self.fields:
+            lines.append('  pass')
+        else:
+            lines.append('  def __init__(self):')
+            for f in self.fields:
+                lines.append('    %s' % f)
+        return '\n'.join(lines)
+
+
+class CodeBuilder:
+    def __init__(self, root_name):
+        self.__class = Class(root_name)
+
+    def add_field(self, type, name):
+        self.__class.fields.append(Field(type, name))
+        return self
+
+    def __str__(self):
+        return self.__class.__str__()
