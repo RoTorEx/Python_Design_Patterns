71c731d128236ae8db0da5794d9dba72e165becb
Aliaksei Stralkou <aliaksei.stralkou@innowise-group.com>
2022-09-16 11:12:18 +0300

    pattern: Bridge: exercise

--- 7_Bridge/exercise.py
+++ 7_Bridge/exercise.py
@@ -95,4 +95,45 @@ class Renderer(ABC):
     def what_to_render_as(self):
         return None
 
-# ToDo: reimplement Shape, Square, Triangle and Renderer/VectorRenderer/RasterRenderer
+
+class Shape(ABC):
+    def __init__(self, renderer, name):
+        self.renderer = renderer
+        self.name = name
+
+    def __str__(self):
+        return 'Drawing %s as %s' % (self.name, self.renderer.what_to_render_as)
+
+
+class Triangle(Shape):
+    def __init__(self, renderer):
+        super().__init__(renderer, 'Triangle')
+
+
+class Square(Shape):
+    def __init__(self, renderer):
+        super().__init__(renderer, 'Square')
+
+
+class RasterRenderer(Renderer):
+    @property
+    def what_to_render_as(self):
+        return 'pixels'
+
+
+class VectorRenderer(Renderer):
+    @property
+    def what_to_render_as(self):
+        return 'lines'
+
+
+rr = RasterRenderer()
+vr = VectorRenderer()
+
+r_square = Square(rr)
+r_triangle = Triangle(rr)
+
+v_square = Square(vr)
+v_triangle = Triangle(vr)
+
+print(f"Using RasterRender:\n- {r_square}\n- {r_triangle}\n\nUsing VectorRender:\n- {v_square}\n- {v_triangle}")
