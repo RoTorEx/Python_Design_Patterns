84a9b5677201cae424677c2f1159f1b6bf89e977
Aliaksei Stralkou <aliaksei.stralkou@innowise-group.com>
2022-09-30 12:58:49 +0300

    pattern: Flyweight: exercise

--- 11_Flyweight/exercise.py
+++ 11_Flyweight/exercise.py
@@ -22,5 +22,36 @@ class Sentence:
 # ?Solution
 class Sentence:
     def __init__(self, plain_text):
-        pass
-        # ToDo
+        self.words = plain_text.split(" ")
+        self.tokens = {}
+
+    def __getitem__(self, item):
+        wt = self.WordToken()
+        self.tokens[item] = wt
+
+        return self.tokens[item]
+
+    class WordToken:
+        def __init__(self, capitalize=False):
+            self.capitalize = capitalize
+
+    def __str__(self):
+        result = []
+
+        for i in range(len(self.words)):
+            w = self.words[i]
+
+            if i in self.tokens and self.tokens[i].capitalize:
+                w = w.upper()
+
+            result.append(w)
+
+        return " ".join(result)
+
+
+sentence = Sentence("Hello world! I'm software engineer :D")
+print(sentence)
+
+sentence[1].capitalize = True
+sentence[3].capitalize = True
+print(sentence)
