8df3902232c60434e077457835a7515cd9650422
Aliaksei Stralkou <aliaksei.stralkou@innowise-group.com>
2022-09-30 12:32:16 +0300

    pattern: Flyweight

---
+++ 11_Flyweight/exercise.py
@@ -0,0 +1,26 @@
+"""Flyweight pattern task
+
+Given a Sentence class whose constructor takes a string (such as "hello world").
+
+You need to implement the API in such a way that the indexer returns a flyweight through which you can translate
+specific words from the string to uppercase. Here is an example of using such an API:
+
+sentence = Sentence('hello world')
+sentence[1].capitalize = True
+print(sentence) # writes 'hello WORLD'"""
+
+
+"""
+# *Start template
+class Sentence:
+    def __init__(self, plain_text):
+        pass
+        # ToDo
+"""
+
+
+# ?Solution
+class Sentence:
+    def __init__(self, plain_text):
+        pass
+        # ToDo
