f4b83179742caafdec4933a35e41d9b862ce2f38
Aliaksei Stralkou <aliaksei.stralkou@innowise-group.com>
2022-10-04 16:34:46 +0300

    pattern: Chain of Responsibility: exercise

--- 13_Chain_of_Responsibility/exercise.py
+++ 13_Chain_of_Responsibility/exercise.py
@@ -20,6 +20,8 @@ Now the characteristics of each goblin are 2/4 (
 The state of all goblins at any given time must be consistent.
 """
 
+from abc import ABC
+from enum import Enum
 
 """
 # *Start template
@@ -42,20 +44,67 @@ class Game:
 
 
 # ?Solution
-class Creature:
-    ...
+class Creature(ABC):
+    def __init__(self, game, attack, defense):
+        self.initial_defense = defense
+        self.initial_attack = attack
+        self.game = game
+
+    @property
+    def attack(self):
+        pass
+
+    @property
+    def defense(self):
+        pass
+
+    def query(self, source, query):
+        pass
+
+
+class WhatToQuery(Enum):
+    ATTACK = 1
+    DEFENSE = 2
 
 
 class Goblin(Creature):
     def __init__(self, game, attack=1, defense=1):
-        # ToDo
-        pass
+        super().__init__(game, attack, defense)
+
+    @property
+    def attack(self):
+        q = Query(self.initial_attack, WhatToQuery.ATTACK)
+        for c in self.game.creatures:
+            c.query(self, q)
+        return q.value
+
+    @property
+    def defense(self):
+        q = Query(self.initial_defense, WhatToQuery.DEFENSE)
+        for c in self.game.creatures:
+            c.query(self, q)
+        return q.value
+
+    def query(self, source, query):
+        if self != source and query.what_to_query == WhatToQuery.DEFENSE:
+            query.value += 1
 
 
 class GoblinKing(Goblin):
     def __init__(self, game):
-        # ToDo
-        pass
+        super().__init__(game, 3, 3)
+
+    def query(self, source, query):
+        if self != source and query.what_to_query == WhatToQuery.ATTACK:
+            query.value += 1
+        else:
+            super().query(source, query)
+
+
+class Query:
+    def __init__(self, initial_value, what_to_query):
+        self.what_to_query = what_to_query
+        self.value = initial_value
 
 
 class Game:
