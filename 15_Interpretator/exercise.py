b86f6798c7f16ad01b38ed193bd3ec573c1b756f
Aliaksei Stralkou <aliaksei.stralkou@innowise-group.com>
2022-10-10 15:07:51 +0300

    pettern: Interpretator: exercise

--- 15_Interpretator/exercise.py
+++ 15_Interpretator/exercise.py
@@ -23,6 +23,10 @@ calculate("10-2-x") if x=3 is in variables = 5
 Translated with www.DeepL.com/Translator (free version)"""
 
 
+import re
+from enum import Enum
+
+
 """
 # *Start template
 class ExpressionProcessor:
@@ -36,10 +40,52 @@ class ExpressionProcessor:
 
 
 # ?Solution
+def megasplit(pattern, string):
+    splits = list((m.start(), m.end()) for m in re.finditer(pattern, string))
+    starts = [0] + [i[1] for i in splits]
+    ends = [i[0] for i in splits] + [len(string)]
+    return [string[start:end] for start, end in zip(starts, ends)]
+
+
 class ExpressionProcessor:
+    class NextOp(Enum):
+        PLUS = 1
+        MINUS = 2
+
     def __init__(self):
         self.variables = {}
 
     def calculate(self, expression):
-        pass
-        # ToDo
+        current = 0
+        next_op = None
+
+        # doesn't work in python 3.5
+        # parts = re.split('(?<=[+-])', expression)
+        parts = megasplit("(?<=[+-])", expression)
+
+        for part in parts:
+            noop = re.split("[\+\-]", part)
+            first = noop[0]
+            value = 0
+
+            try:
+                value = int(first)
+            except ValueError:
+                if len(first) == 1 and first[0] in self.variables:
+                    value = self.variables[first[0]]
+                else:
+                    return 0
+
+            if not next_op:
+                current = value
+            elif next_op == self.NextOp.PLUS:
+                current += value
+            elif next_op == self.NextOp.MINUS:
+                current -= value
+
+            if part.endswith("+"):
+                next_op = self.NextOp.PLUS
+            elif part.endswith("-"):
+                next_op = self.NextOp.MINUS
+
+        return current
