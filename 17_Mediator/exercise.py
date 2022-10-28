ff0ede267ac15a42e4f5e30fc3d1c183182691bf
Aliaksei Stralkou <aliaksei.stralkou@innowise-group.com>
2022-10-28 13:56:02 +0400

    pattern: Mediator: exercise

--- 17_Mediator/exercise.py
+++ 17_Mediator/exercise.py
@@ -29,11 +29,29 @@ class Participant:
 
 
 # ?Solution
+class Event(list):
+    def __call__(self, *args, **kwargs):
+        for item in self:
+            item(*args, **kwargs)
+
+
 class Participant:
     def __init__(self, mediator):
         self.value = 0
         self.mediator = mediator
+        mediator.alert.append(self.mediator_alert)
+
+    def mediator_alert(self, sender, value):
+        if sender != self:
+            self.value += value
 
     def say(self, value):
-        pass
-        # ToDo
+        self.mediator.broadcast(self, value)
+
+
+class Mediator:
+    def __init__(self):
+        self.alert = Event()
+
+    def broadcast(self, sender, value):
+        self.alert(sender, value)
