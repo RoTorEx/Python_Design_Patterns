3081daf2ce7e781cdf2cbbb7f324bcc78b24d6d4
Aliaksei Stralkou <aliaksei.stralkou@innowise-group.com>
2022-11-04 13:24:49 +0400

    pattern: Template Method: exersice

--- 22_Template_Method/exercise.py
+++ 22_Template_Method/exercise.py
@@ -72,18 +72,28 @@ class CardGame(ABC):
     # return -1 if both creatures alive or both dead after combat
     # otherwise, return the _index_ of winning creature
     def combat(self, c1_index, c2_index):
-        pass
-        # ToDo
+        first = self.creatures[c1_index]
+        second = self.creatures[c2_index]
+        self.hit(first, second)
+        self.hit(second, first)
+        first_alive = first.health > 0
+        second_alive = second.health > 0
+        if first_alive == second_alive:
+            return -1
+        return c1_index if first_alive else c2_index
 
     def hit(self, attacker, defender):
         pass  # implement this in derived classes
 
 
 class TemporaryDamageCardGame(CardGame):
-    pass
-    # ToDo
+    def hit(self, attacker, defender):
+        old_health = defender.health
+        defender.health -= attacker.attack
+        if defender.health > 0:
+            defender.health = old_health
 
 
 class PermanentDamageCardGame(CardGame):
-    pass
-    # ToDo
+    def hit(self, attacker, defender):
+        defender.health -= attacker.attack
