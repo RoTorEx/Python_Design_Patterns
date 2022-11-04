c54e0da742b97648c36191f5b649e2195f25fdd4
Aliaksei Stralkou <aliaksei.stralkou@innowise-group.com>
2022-11-04 13:20:04 +0400

    pattern: Template Method

---
+++ 22_Template_Method/exercise.py
@@ -0,0 +1,89 @@
+"""Template Method pattern task
+
+Imagine a typical collectible card game in which cards represent creatures.
+Each creature has two attributes: attack and health. Creatures can fight by dealing damage equal to their attack value
+(reducing the health of the creature being attacked).
+
+The CardGame class implements the logic for two fighting creatures. However, the exact mechanics of dealing damage
+may vary:
+
+TemporaryCardDamage : in some games (e.g. Magic: the Gathering), until a creature is destroyed, its health is restored
+to its original level at the end of each combat.
+
+PermanentCardDamage : in other games (e.g. Hearthstone), the damage dealt is saved.
+
+Implement classesTemporaryCardDamageGame and PermanentCardDamageGame that implement the described logic.
+
+A couple of examples:
+
+In temporary damage mode, two creatures with characteristics 1/2 and 1/3 can never kill each other.
+In permanent damage mode, the second creature wins in two rounds.
+
+In either of the two modes, two creatures with characteristics 2/2 will kill each other."""
+
+
+from abc import ABC
+
+
+"""
+# *Start Template
+class Creature:
+    def __init__(self, attack, health):
+        self.health = health
+        self.attack = attack
+
+
+class CardGame(ABC):
+    def __init__(self, creatures):
+        self.creatures = creatures
+
+    # return -1 if both creatures alive or both dead after combat
+    # otherwise, return the _index_ of winning creature
+    def combat(self, c1_index, c2_index):
+        pass
+        # ToDo
+
+    def hit(self, attacker, defender):
+        pass  # implement this in derived classes
+
+
+class TemporaryDamageCardGame(CardGame):
+    pass
+    # ToDo
+
+
+class PermanentDamageCardGame(CardGame):
+    pass
+    # ToDo
+"""
+
+
+# ?Solution
+class Creature:
+    def __init__(self, attack, health):
+        self.health = health
+        self.attack = attack
+
+
+class CardGame(ABC):
+    def __init__(self, creatures):
+        self.creatures = creatures
+
+    # return -1 if both creatures alive or both dead after combat
+    # otherwise, return the _index_ of winning creature
+    def combat(self, c1_index, c2_index):
+        pass
+        # ToDo
+
+    def hit(self, attacker, defender):
+        pass  # implement this in derived classes
+
+
+class TemporaryDamageCardGame(CardGame):
+    pass
+    # ToDo
+
+
+class PermanentDamageCardGame(CardGame):
+    pass
+    # ToDo
